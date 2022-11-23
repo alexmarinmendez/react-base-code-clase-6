ee5947d362db5e56140d0abd48835c05926164dc
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-11-23 18:44:15 -0500

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 const Navbar = () => {
+    //componentDidMount
+    useEffect(() => {
+        console.log('El componente Navbar se montó en el DOM....')
+    }, [])
+
     return (
         <header>
             <strong><em>React-Tube-App</em></strong>

3e8d03978c1718c313714bebf9952a097530e509
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-11-23 20:12:12 -0500

    feat: add a promises for fetching data

--- src/containers/Videos.js
+++ src/containers/Videos.js
@@ -1,11 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Video from '../components/Video';
 // import Video from '../components/VideoClass';
 // import Video from '../components/VideoClassES7';
 import { data } from '../utils/data';
+import { fetchData } from '../utils/fetchData';
 
 const Videos = () => {
-  const [datos, setDatos] = useState(data);
+  const [datos, setDatos] = useState([]);
+
+  //componentDidMount
+  useEffect(() => {
+    fetchData(2000, data)
+      .then(response => setDatos(data))
+      .catch(err => console.log(err))
+  }, [])
 
   const deleteItem = (id) => {
     let newData = datos.filter(item => item.id !== id)
